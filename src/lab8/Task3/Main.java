0137ee6ba6e5377ab37b1620f9e4edeeff5a2288
PrzemekCraker <przemekzbigniew@gmail.com>
2021-05-07 12:56:54 +0200

    Lab08

---
+++ src/lab8/Task3/Main.java
@@ -0,0 +1,9 @@
+package lab8.Task3;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String[] months = {"Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"};
+    }
+}
